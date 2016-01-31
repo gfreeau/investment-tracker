4bba016d453675d46e41c510d234a74ff94c346f
gfreeau <greg@imagize.com.au>
2016-01-30 21:30:51 -0500

    Clean code

--- src/Gfreeau/Portfolio/Processor.php
+++ src/Gfreeau/Portfolio/Processor.php
@@ -24,7 +24,48 @@ class Processor
      */
     public function process(array $config, array $contributionConfig = null): Portfolio
     {
-        $assetClasses = $config['assetClasses'];
+        $assetClasses = $this->processAssetClasses($config['assetClasses']);
+
+        if ($contributionConfig) {
+            $config = $this->processContribution($config, $contributionConfig);
+        }
+
+        $accountData = $config['accounts'];
+
+        $symbols = $this->getAllStockSymbols($accountData);
+        $prices = $this->getStockPrices($symbols);
+
+        // make a copy for use with array_map
+        $accounts = $accountData;
+
+        // references are important
+        foreach($accounts as $name => &$account) {
+            foreach($account['holdings'] as &$holding) {
+                // overwrite value
+                $holding = new Holding(
+                    $assetClasses[$holding['assetClass']],
+                    $holding['name'],
+                    $holding['symbol'],
+                    $holding['quantity'],
+                    $prices[$holding['symbol']]
+                );
+            }
+
+            // overwrite value
+            $account = new Account(
+                $name,
+                $account['cash'],
+                $account['holdings']
+            );
+
+            unset($name, $account, $holding);
+        }
+
+        return new Portfolio($assetClasses, $accounts);
+    }
+
+    protected function processAssetClasses(array $assetClasses): array
+    {
         $totalAllocation = 0;
 
         foreach($assetClasses as $name => &$value) {
@@ -42,93 +83,57 @@ class Processor
             throw new \RuntimeException('total allocation is greater than 100%');
         }
 
-        $accountData = $config['accounts'];
-
-        if ($contributionConfig) {
-            $contributionConfig = $this->processContribution($contributionConfig, (float) $config['tradingFee']);
-
-            foreach($contributionConfig['accounts'] as $name => $tempAccount) {
-                if (!array_key_exists($name, $accountData)) {
-                    continue;
-                }
-
-                $thisAccount = &$accountData[$name];
-
-                if (!array_key_exists('cash', $thisAccount)) {
-                    $thisAccount['cash'] = 0;
-                }
-
-                $thisAccount['cash'] += $tempAccount['unusedContribution'];
-
-                $thisAccount['holdings'] = array_merge($thisAccount['holdings'], $tempAccount['holdings']);
-
-                unset($thisAccount);
-            }
-
-            unset($name, $tempAccount);
-        }
-
-        $symbols = $this->getAllStockSymbols($accountData);
-        $prices = $this->getStockPrices($symbols);
-
-        $accounts = $accountData;
-
-        foreach($accounts as $name => &$tempAccount) {
-            foreach($tempAccount['holdings'] as &$holding) {
-                // overwrite value
-                $holding = new Holding(
-                    $assetClasses[$holding['assetClass']],
-                    $holding['name'],
-                    $holding['symbol'],
-                    $holding['quantity'],
-                    $prices[$holding['symbol']]
-                );
-            }
-
-            // overwrite value
-            $tempAccount = new Account(
-                $name,
-                $tempAccount['cash'],
-                $tempAccount['holdings']
-            );
-
-            unset($name, $tempAccount, $holding);
-        }
-
-        return new Portfolio($assetClasses, $accounts);
+        return $assetClasses;
     }
 
     /**
      * @param array $config
-     * @param float $tradingFee
+     * @param array $contributionConfig
      * @return array
      * @throws ContributionExceededException
      */
-    protected function processContribution(array $config, float $tradingFee): array
+    protected function processContribution(array $config, array $contributionConfig): array
     {
-        $accountData = $config['accounts'];
+        // reference is important
+        $mainAccountData = &$config['accounts'];
+        $contributionAccountData = $contributionConfig['accounts'];
 
-        $symbols = $this->getAllStockSymbols($accountData);
+        $symbols = $this->getAllStockSymbols($contributionAccountData);
         $prices = $this->getStockPrices($symbols);
 
-        foreach($config['accounts'] as $name => &$account) {
+        foreach($contributionAccountData as $name => $account) {
+            if (!array_key_exists($name, $mainAccountData)) {
+                continue;
+            }
+
             $contribution = (float) $account['contribution'];
             $cost = 0;
             $fees = 0;
 
             foreach($account['holdings'] as $holding) {
                 $cost += $holding['quantity'] * $prices[$holding['symbol']];
-                $fees += $tradingFee;
+                $fees += $config['tradingFee'];
             }
 
             $cost += $fees;
 
             // todo factor in any existing cash in the account
             if ($cost > $contribution) {
-                throw new ContributionExceededException(sprintf('The contribution of %4.2f exceeds the contribution of %4.2f to the "%s" account', $cost, $contribution, $name));
+                throw new ContributionExceededException(sprintf('The cost of $%4.2f exceeds the contribution of $%4.2f to the "%s" account', $cost, $contribution, $name));
             }
 
-            $account['unusedContribution'] = ($contribution - $cost);
+            $unusedContribution = $contribution - $cost;
+
+            // reference is important
+            $mainAccount = &$mainAccountData[$name];
+
+            if (!array_key_exists('cash', $mainAccount)) {
+                $mainAccount['cash'] = 0;
+            }
+
+            $mainAccount['cash'] += $unusedContribution;
+
+            $mainAccount['holdings'] = array_merge($mainAccount['holdings'], $account['holdings']);
         }
 
         return $config;
